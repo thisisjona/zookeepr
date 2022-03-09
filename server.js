b3893d4c618e5f8a028285c464f7ec4bda0c689c
thisisjona <thisisjona@gmail.com>
2022-03-08 22:13:00 -0600

    linked frontend to back end

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded({extended: true}));
 //parse incoming json data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   let filteredResults = animalsArray;
@@ -101,7 +103,23 @@ app.post('/api/animals', (req, res) => {
   const animal = createNewAnimal(req.body, animals);
 
   res.json(req.body);
-})
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
